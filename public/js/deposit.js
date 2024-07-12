3e68d537aec0b121cd021248338e9d15e53a3f03
Sakthi (Sandy) Santhosh <166415082+ssanthosh010303@users.noreply.github.com>
2024-07-12 03:22:37 +0000

    v1.1.0-alpha: complete deposit and withdrawal logic

--- public/js/deposit.js
+++ public/js/deposit.js
@@ -2,19 +2,22 @@
  * Author: Bank Bros
  * Created on: 11/07/2024
 */
-const url = "http://localhost:"
+const url = "http://localhost:xxxx"
 const formElement = document.querySelector("form");
 
 async function getData(data) {
     const response = fetch(
-        "", {
+        url, {
             method: "POST",
             body: data
         }
     );
 
-    if (response.status != 200)
-        throw new Error(response.status);
+    if (response.status == 401)
+        throw new Error("Invalid pin entered.");
+
+    if (response.staus >= 500)
+        throw new Error("The server cannot process the request right now.");
 
     return (await response).json();
 }
@@ -31,10 +34,10 @@ function handleFormSubmission(event) {
 
     getData(data)
     .then(result => {
-        alert("Your amount has been credited successfully.");
+        alert("Your money has been deposited successfully.");
     })
     .catch(error => {
-        alert("Account creation failed with the reason: " + error);
+        alert("Error: " + error);
     });
 }
 
--- public/js/withdraw.js
+++ public/js/withdraw.js
@@ -2,3 +2,43 @@
  * Author: Bank Bros
  * Created on: 11/07/2024
 */
+const url = "http://localhost:xxxx"
+const formElement = document.querySelector("form");
+
+async function getData(data) {
+    const response = fetch(
+        url, {
+            method: "POST",
+            body: data
+        }
+    );
+
+    if (response.status == 401)
+        throw new Error("Invalid pin entered.");
+
+    if (response.staus >= 500)
+        throw new Error("The server cannot process the request right now.");
+
+    return (await response).json();
+}
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+
+    const formData = new FormData(formElement);
+    const data = {
+        amount: formData.get("amount"),
+        cardNumber: formData.get("cardno"),
+        pin: formData.get("pin")
+    };
+
+    getData(data)
+    .then(result => {
+        alert("Your money has been credited successfully.");
+    })
+    .catch(error => {
+        alert("Error: " + error);
+    });
+}
+
+formElement.addEventListener("submit", handleFormSubmission);
